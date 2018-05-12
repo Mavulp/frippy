8e4b07b759004793e1308ae2f98d18703878e27c
Jokler <jokler.contact@gmail.com>
2018-05-12 17:18:25 +0200

    Help: Send a static list of available commands

--- src/plugins/help.rs
+++ src/plugins/help.rs
@@ -26,7 +26,9 @@ impl Plugin for Help {
 
     fn command(&self, client: &IrcClient, command: PluginCommand) -> Result<(), FrippyError> {
         Ok(client
-            .send_notice(&command.source, "Help has not been added yet.")
+            .send_notice(&command.source, "Available commands: help, currency, tell, factoids\r\n\
+                For more detailed help call help on the specific command.\r\n\
+                Example: 'currency help'")
             .context(FrippyErrorKind::Connection)?)
     }
 

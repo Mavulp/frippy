448b52d250c61fb719477c01c633593c3da68fba
Jokler <jokler.contact@gmail.com>
2018-09-21 00:19:40 +0200

    Errors: Fix accidental double printing

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@ use failure::Fail;
 
 pub fn log_error(e: &FrippyError) {
     let text = e.causes()
+        .skip(1)
         .fold(format!("{}", e), |acc, err| format!("{}: {}", acc, err));
     error!("{}", text);
 }
